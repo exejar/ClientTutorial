a1fcee3052ca40e2c147970a7aeac9ed88650feb
exejar <themmludwig@gmail.com>
2021-06-16 17:08:46 -0400

    Initial commit

---
+++ src/main/java/me/exejar/clienttutorial/Main.java
@@ -0,0 +1,10 @@
+package me.exejar.clienttutorial;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = "clienttutorial", name = "ClientTutorial", clientSideOnly = true, version = "1.0", acceptedMinecraftVersions = "1.8.9")
+public class Main {
+
+
+
+}
